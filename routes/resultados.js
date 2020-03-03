f2294cccffebece26c7fb0357981330066d5f715
Kise <ruosena@gmail.com>
2020-03-03 09:05:32 -0300

    banca results route

--- routes/resultados.js
+++ routes/resultados.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
+const moment = require("moment");
 const Scraper = require("../middlewares/resultsScraper/main");
 
 router.get("/", async function(req, res, next) {
-    Scraper(null, null, (erro, ultimos_resultados) => {
-        if (erro) {return next(erro)}
-        res.render('sorteios', {title: ':: Últimos Resultados ::', resultados: ultimos_resultados});
+  Scraper(null, null, (erro, ultimos_resultados) => {
+    if (erro) {
+      return next(erro);
+    }
+    res.render("sorteios", {
+      title: ":: Últimos Resultados ::",
+      resultados: ultimos_resultados
     });
+  });
+});
+
+router.get("/:banca", function(req, res, next) {
+  let data = new Date();
+  data = moment(data).format("DD_MM_YYYY");
+  res.send(data);
+});
+
+router.get("/:banca/:dia", function(req, res, next) {
+  Scraper(req.params.banca, req.params.dia, (erro, resultados_dia) => {
+    if (erro) {
+      return next(erro);
+    }
+    res.render("sorteios_banca", {
+      title: `:: ${req.params.dia} ::`,
+      resultados: resultados_dia
+    });
+  });
 });
 
 module.exports = router;
